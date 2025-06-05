0bc8b6fd86dbce86652bc2792aa0e998c98d567f
XiaomengDong <xiaomend@uci.edu>
2025-06-04 17:41:41 -0700

    Connect to vercel deployed frontend

--- medimate/backend/server.js
+++ medimate/backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.use(cors({
-  origin: 'https://medimate-frontend-p5hx.onrender.com',
+  origin: 'https://medimate-chi.vercel.app',
   credentials: true
 }));
 app.use(express.json());
